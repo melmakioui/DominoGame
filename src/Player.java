19c9c5a2dc0ea499a7d9bc691c1cab6c1f78245d
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-05-29 15:12:01 +0200

    Creacio classe Player e implementacio

---
+++ src/Player.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private List<Tile> hand;
+    private String name;
+    private int points;
+
+    public Player(String name){
+        this.hand = new ArrayList<>();
+        this.name = name;
+    }
+
+    public void addTile(Tile tile){
+        hand.add(tile);
+    }
+
+    public void removeTile(Tile tile) {
+        hand.remove(tile);
+    }
+
+    public boolean isHandEmpty(){
+        return this.hand.isEmpty();
+    }
+
+    public void addPoints(int points){
+        this.points += points;
+    }
+
+    @Override
+    public String toString() {
+        return name + "Tiles --> " + hand;
+    }
+}
