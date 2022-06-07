5776899bde2514098988529dbf89b59ee05fcf47
Mohamed el Makioui <melmakioui@outlook.es>
2022-06-07 16:15:43 +0200

    Creacui classe Team e implementacio

--- src/Game/Team.java
+++ src/Game/Team.java
@@ -1,13 +1,42 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Team {
 
-    private ArrayList<Player> team;
+    private String teamName;
+    private List<Player> teamPlayers;
+    private int points;
 
-    public Team() {
-
+    public Team(int numTeam) {
+        this.teamName = "Team " + numTeam;
+        this.teamPlayers = new ArrayList<>();
+        this.points = 0;
     }
 
+    public void addPlayer(Player player){
+        teamPlayers.add(player);
+    }
+
+    public void addPoints(int points){
+        this.points += points;
+    }
+
+    public int getPoints (){
+        return points;
+    }
+
+    public List<Player> getTeamPlayers(){
+        return teamPlayers;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    @Override
+    public String toString() {
+        return teamName  + "has --> " + points;
+    }
 }
